7f3e1ae242c3c533e2ed1efd4a9b4cfc0d56aae8
Gabriel Avila <gabriel.avila@senior.com.br>
2022-03-04 22:01:26 -0300

    Corrigido detalhes de padrão

--- src/main/java/com/kiguel/rest_controllers/PlayerRestController.java
+++ src/main/java/com/kiguel/rest_controllers/PlayerRestController.java
@@ -1,55 +1,57 @@
-package com.kiguel.controllers;
+package com.kiguel.rest_controllers;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.kiguel.controllers.PlayerController;
 import com.kiguel.entities.PlayerEntity;
-import com.kiguel.repositories.PlayerRepository;
 
-@Controller
-public class PlayerController {
+@RestController
+@RequestMapping("/players")
+public class PlayerRestController {
 	
 	@Autowired
-	private PlayerRepository playerRepository;
+	private PlayerController playerController;
 
 	@GetMapping
 	public List<PlayerEntity> getAll() {
-		return playerRepository.findAll();
+		return playerController.getAll();
 	}
 
 	@GetMapping("/{idPlayer}")
 	public Optional<PlayerEntity> getById(@PathVariable Long idPlayer) {
-		return playerRepository.findById(idPlayer);
+		return playerController.getById(idPlayer);
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public PlayerEntity save(@RequestBody PlayerEntity player) {
-		return playerRepository.save(player);
+		return playerController.save(player);
 	}
 
 	@PutMapping("/{idPlayer}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public PlayerEntity edit(@PathVariable Long idPlayer, @RequestBody PlayerEntity player) {
 		player.setId(idPlayer);
-		return playerRepository.save(player);
+		return playerController.save(player);
 	}
 	
 	@DeleteMapping("/{idPlayer}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long idPlayer) {
-//		PlayerEntity player = playerRepository.getById(idPlayer);
-//		playerRepository.delete(player);
+//		PlayerEntity player = playerController.getById(idPlayer);
+//		playerController.delete(player);
 	}
 }
